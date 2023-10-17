136398d156b154e9b93950b9e4f833fef3df4f86
ahmed shehata <ahamdyshehata7@gmail.com>
2023-10-17 11:13:49 +0300

    Fix screen header text

--- app/src/main/java/com/ashehata/diabetes_task/features/diabetes/presentation/composables/WelcomeHeader.kt
+++ app/src/main/java/com/ashehata/diabetes_task/features/diabetes/presentation/composables/WelcomeHeader.kt
@@ -2,15 +2,14 @@ package com.ashehata.diabetes_task.features.diabetes.presentation.composables
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ashehata.diabetes_task.R
@@ -20,30 +19,20 @@ import com.ashehata.diabetes_task.util.getCurrentTime
 @Composable
 fun WelcomeHeader(userEmail: String) {
     Column(Modifier.padding(16.dp), verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        val currentDate = remember { getCurrentTime() }
 
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Text(
-                text = stringResource(id = R.string.welcome),
-                style = MaterialTheme.typography.subtitle2.copy(
-                    fontSize = 20.sp
-                )
-            )
-            Text(text = userEmail, modifier = Modifier.weight(1f))
-        }
+        Text(
+            text = String.format(stringResource(id = R.string.welcome), userEmail),
+            style = MaterialTheme.typography.subtitle2.copy(fontSize = 20.sp),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
 
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            val currentDate = remember { getCurrentTime() }
-
-            Text(
-                text = stringResource(id = R.string.login_at),
-                style = MaterialTheme.typography.subtitle2.copy(
-                    fontSize = 20.sp
-                )
-            )
-            Text(text = currentDate, modifier = Modifier.weight(1f))
-        }
-
+        Text(
+            text = String.format(stringResource(id = R.string.login_at), currentDate),
+            style = MaterialTheme.typography.subtitle2.copy(fontSize = 20.sp),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
     }
-
-
 }
